73b0e03bcc6fb0b32212c0f2472a35894f481ee3
Core Swety <swety2003@outlook.com>
2022-05-06 21:31:35 +0800

    更新

---
+++ Dialogs/AlermProc.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Project.Dialogs
+{
+    /// <summary>
+    /// AlermProc.xaml 的交互逻辑
+    /// </summary>
+    public partial class AlermProc : Window
+    {
+        string id;
+        public AlermProc(string id)
+        {
+            InitializeComponent();
+            this.id = id;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
